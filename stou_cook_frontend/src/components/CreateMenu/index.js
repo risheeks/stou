42c49bfe442b252be83dea4c778f37e338a2e85c
Soumil Uppal <soumil.barca@gmail.com>
2019-09-29 14:01:17 -0400

    Created folder structure

---
+++ stou_cook_frontend/src/components/CreateMenu/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+export class CreateMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Form
+            </div>
+        );
+    }
+}
