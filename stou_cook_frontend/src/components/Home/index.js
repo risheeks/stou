d29b201429d96d066220d09adc9b3d00b5cd111b
Soumil Uppal <soumil.barca@gmail.com>
2019-09-29 16:55:54 -0400

    Navbar added

---
+++ stou_cook_frontend/src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../Common/Header';
+import '../../styles/Main.css';
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default Home;
