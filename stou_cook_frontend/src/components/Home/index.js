435f2fb4cd1c95be4736b7c53d7d94cfb56af591
Risheek N <nr@pal-nat186-177-125.itap.purdue.edu>
2019-09-30 14:18:26 -0400

    register fix

--- stou_cook_frontend/src/components/Home/index.js
+++ stou_cook_frontend/src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Login from '../Login';
+import Register from '../Register';
 
 export class Home extends Component {
     render() {
         return (
             <div>
                 <Login />
+                <Register/>
             </div>
         );
     }
