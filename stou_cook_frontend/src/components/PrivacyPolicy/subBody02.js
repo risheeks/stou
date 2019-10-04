ca9cbd9b92a21d5550a5a62b7e8f35726cd3547b
Risheek N <risheeks@gmail.com>
2019-10-04 03:34:34 -0400

    privacy

---
+++ stou_cook_frontend/src/components/PrivacyPolicy/subBody02.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './index.css'
+import { Container } from "react-bootstrap";
+
+
+
+const SubBody02 = () => {
+	return(
+		<Container className = 'subBody'>
+		 	<h2>Use of information collected from users</h2>
+			<p>We enhance user experience in multiple ways. However, that requires us to collect information. We use the information in the following ways.</p>
+			<ul>
+				<li>Improve our service.</li>
+				<li>Promote our application.</li>
+				<li>Communication with users.</li>
+				<li>Prevention of fraud.</li>
+			</ul>
+		</Container>
+	)	
+}
+export default SubBody02
