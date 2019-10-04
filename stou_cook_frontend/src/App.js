0311d5d81e0264d71a9f9360ea06af59fc3ed88e
Soumil Uppal <soumil.barca@gmail.com>
2019-10-03 23:22:28 -0400

    Modal redux

--- stou_cook_frontend/src/App.js
+++ stou_cook_frontend/src/App.js
@@ -5,6 +5,9 @@ import Register from './components/Register';
 import './styles/Main.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 
 import Login from './components/Login';
 import Home from './components/Home';
@@ -12,18 +15,21 @@ import Header from './components/Common/Header';
 import AddFoodItem from './components/AddFoodItem';
 import Profile from './components/Profile';
 
+const rootStore = createStore(rootReducer);
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Route exact={true} path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/addfood" component={AddFoodItem} />
-        <Route path="/profile" component={Profile} />
-      </Router>
+      <Provider store={rootStore}>
+        <Router>
+          <Header />
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/addfood" component={AddFoodItem} />
+          <Route path="/profile" component={Profile} />
+        </Router>
+      </Provider>
     </div>
   );
 }
