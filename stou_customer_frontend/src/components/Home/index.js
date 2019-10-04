ef3f6d0a00f919ecdc4861ac9e1cfcd92bb4293f
Risheek N <risheeks@gmail.com>
2019-10-04 06:38:16 -0400

    home fix

--- stou_customer_frontend/src/components/Home/index.js
+++ stou_customer_frontend/src/components/Home/index.js
@@ -5,21 +5,57 @@ import FilterBar from '../FilterBar';
 import "../../styles/Main.css";
 import ViewFoodOptions from '../ViewFoodOptions';
 import ListOfHomeCooks from '../ListOfHomeCooks';
-import { Container, ListGroup } from 'react-bootstrap';
+import axios from 'axios';
+import { Container, ListGroup, Form, Button } from 'react-bootstrap';
 
 export class Home extends Component {
+
+    constructor(props){
+		super(props);
+		this.state = {
+			zip: ""
+		}
+
+	}
+	sendLocation = e => {
+		e.preventDefault();
+		axios.post("http://192.168.43.177:300/updateZip",{
+			params: {
+				zip: this.state.zip
+			}
+		})
+	}
+
     render() {
         return (
-            <div className="homec">
-                <FilterBar />
+            <div className="home">
+                <div className="location-zipcode">
+                    <Form>
+                        <Form.Group controlId="formGroupEmail">
+                            <Form.Label>Location</Form.Label>
+                            <Form.Control type="email" placeholder="Enter zipcode" />
+                        </Form.Group>
+                       </Form>
+                    <Button
+                        block
+                        bsSize="large"
+                        className="submit-button"
+                        onClick={this.sendLocation}>
+                        Submit
+                        </Button>
+                </div>
                 <div className="homec">
-                    <div className="home">
-                        <h5>Food Available</h5>
-                        <ViewFoodOptions/>
-                    </div>
-                    <div className="home">
-                        <h5>Home Cooks</h5>
-                        <ListOfHomeCooks/>
+                    
+                    <FilterBar />
+                    <div className="homec">
+                        <div className="home">
+                            <h5>Food Available</h5>
+                            <ViewFoodOptions/>
+                        </div>
+                        <div className="home">
+                            <h5>Home Cooks</h5>
+                            <ListOfHomeCooks/>
+                        </div>
                     </div>
                 </div>
             </div>
