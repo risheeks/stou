5f7f1845e03b6d4ed61d3f87fbcc1b74548711ae
maskman007 <maskman.sexy@gmail.com>
2019-10-03 22:23:10 -0400

    vfo fixed

--- stou_customer_frontend/src/components/ViewFoodOptions/index.js
+++ stou_customer_frontend/src/components/ViewFoodOptions/index.js
@@ -1,28 +1,49 @@
 import React from 'react'
-import { Row, Col, Container, Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
-
-import './index.css'
-
+import { Row, Col, Container, Button, ListGroup, FormControl, FormLabel, Image } from "react-bootstrap";
 const ViewFoodOptions = () => {
 	return(
-    <Container className="ViewFoodOptions">
-        <Row>
-            <Col>   
-            <img className="vfo-image rounded float-left" src="https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/mccormick/f/800/fiesta_tacos_800x800.jpg" alr=""></img>
-            </Col>
-            <Col>  
-                <Row className="vfo-chefname">
-                <p>Siddhant Patel</p>
-                </Row>
-                <Row className="vfo-description">
-                <p>tasty italian food lolololololololol</p>
-                </Row>
-            </Col>
-            <Col className="vfo-price">  
-            $$ 10
-            </Col>
-        </Row>
-    </Container>
+	<Container className="ViewFoodOptions">
+	<ListGroup>
+	<ListGroup.Item className="food-option">
+			<div className="food-option-inner">   
+				<div>
+				<Image rounded className="vfo-image" src="https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/mccormick/f/800/fiesta_tacos_800x800.jpg" />
+				</div>
+				<div className="vfo-info">
+					<div className="vfo-foodname">
+					<p>Chilli Tacos</p>
+					</div>
+					<div className="vfo-description">
+					<p>Tasty texmex spicy low calorie taco</p>
+					</div>
+					<div className="vfo-chefname">
+					<p>Siddhant Patel</p>
+					</div>
+				</div>
+				<p className="vfo-price">$10</p> 
+			</div>
+		</ListGroup.Item>
+		<ListGroup.Item className="food-option">
+		<div className="food-option-inner">   
+				<div>
+				<Image rounded className="vfo-image" src="https://d1doqjmisr497k.cloudfront.net/-/media/mccormick-us/recipes/mccormick/f/800/fiesta_tacos_800x800.jpg" />
+				</div>
+				<div className="vfo-info">
+					<div className="vfo-foodname">
+					<p>Chilli Tacos</p>
+					</div>
+					<div className="vfo-description">
+					<p>Tasty texmex spicy low calorie taco</p>
+					</div>
+					<div className="vfo-chefname">
+					<p>Siddhant Patel</p>
+					</div>
+				</div>
+				<p className="vfo-price">$10</p> 
+			</div>
+		</ListGroup.Item>
+	</ListGroup>
+	</Container>
 	)
 	
 }
