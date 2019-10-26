711379136387fa4aa3e3cc0cb986caada58fda3a
Akshay <akshay3398@gmail.com>
2019-10-26 18:04:26 -0400

    Bug fix on FavHomeCook

--- stou_customer_frontend/src/components/Home/index.js
+++ stou_customer_frontend/src/components/Home/index.js
@@ -5,7 +5,6 @@ import FilterBar from '../FilterBar';
 import "../../styles/Main.css";
 import ViewFoodOptions from '../ViewFoodOptions';
 import ListOfHomeCooks from '../ListOfHomeCooks';
-import FavoriteHomeCooksList from '../FavoriteHomeCooks'
 import axios from 'axios';
 import { serverURL } from '../../config';
 import { Container, ListGroup, Form, Button, Modal, Tabs, Tab } from 'react-bootstrap';
@@ -70,7 +69,7 @@ export class Home extends Component {
                         </div>
                     </Tab>
                 </Tabs>
-                <FavoriteHomeCooksList/>
+                
             </div>
         );
     }
