db28c8a71fe2b1576e58c2670061760791af8014
sidBOT <si12hant@gmail.com>
2019-10-27 16:53:09 -0400

    profile

--- stou_customer_frontend/src/config/index.js
+++ stou_customer_frontend/src/config/index.js
@@ -1,1 +1,11 @@
 export const serverURL = `http://localhost:5000`;
+export const firebaseConfig = {
+    apiKey: "AIzaSyCKRmXkIQqNtPTM-_MMvsQYMH1tSm7IlNM",
+    authDomain: "stou-79b9a.firebaseapp.com",
+    databaseURL: "https://stou-79b9a.firebaseio.com",
+    projectId: "stou-79b9a",
+    storageBucket: "stou-79b9a.appspot.com",
+    messagingSenderId: "135234417719",
+    appId: "1:135234417719:web:a6233dfcab2935a2e67bb2",
+    measurementId: "G-EWZ35B7N17"
+  };
