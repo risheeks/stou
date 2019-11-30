58034890039a131656866c53e93a454d9251277c
Soumil Uppal <soumil.barca@gmail.com>
2019-11-30 18:32:43 -0500

    User list done

---
+++ stou_admin_frontend/src/reducers/login.reducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+    auth_token: null,
+    email: null,
+    location: null
+}
+
+const loginReducer = function login(state = initialState, action) {
+    switch (action.type) {
+        // Push user authentication token to state
+        case 'GET_AUTH_TOKEN': {
+            return state = {
+                ...state,
+                auth_token: action.auth_token,
+                email: action.email
+            }
+        }
+        // Sign out of currently logged in account
+        case 'SIGN_OUT': {
+            return state = {
+                ...state,
+                auth_token: null,
+                email: null
+            }
+        }
+        case 'CHANGE_LOCATION': {
+            return state = {
+                ...state,
+                zipcode: action.zipcode
+            }
+        }
+        default:
+            return state;
+    }
+}
+
+export default loginReducer;
