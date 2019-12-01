b9e2a8ffe97611bf2a06ff2b5fc663d1a18fae83
Soumil Uppal <soumil.barca@gmail.com>
2019-11-30 21:24:18 -0500

    Fixed customer chat

--- stou_customer_frontend/src/components/Chat/Maximized.js
+++ stou_customer_frontend/src/components/Chat/Maximized.js
@@ -38,143 +38,144 @@ const getAvatarForUser = (userId, users) => {
 
 const parseUrl = (url) => url && 'https://' + url.replace(/^(http(s)?\:\/\/)/, '').replace(/^\/\//, '')
 
-class Maximized extends React.Component{
+class Maximized extends React.Component {
 
-// const Maximized = ({
-// 	chatState,
-// 	events,
-// 	onMessageSend,
-// 	users,
-// 	ownId,
-// 	currentAgent,
-// 	minimize,
-// 	maximizeChatWidget,
-// 	sendMessage,
-// 	rateGood,
-// 	rateBad,
-//     rate,
-//     messages
-// }) => {
+	// const Maximized = ({
+	// 	chatState,
+	// 	events,
+	// 	onMessageSend,
+	// 	users,
+	// 	ownId,
+	// 	currentAgent,
+	// 	minimize,
+	// 	maximizeChatWidget,
+	// 	sendMessage,
+	// 	rateGood,
+	// 	rateBad,
+	//     rate,
+	//     messages
+	// }) => {
 
 	constructor() {
-        super()
-        this.state = {
-			
-        }
-    } 
+		super()
+		this.state = {
+			textMessage: '',
+		}
+	}
 
 	render() {
-	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				height: '100%',
-			}}
-		>
-			<TitleBar
-				rightIcons={[
-					<IconButton key="close" onClick={this.props.minimize}>
-						<CloseIcon />
-					</IconButton>,
-				]}
-				title="Welcome to LiveChat"
-			/>
-			{this.props.email && (
-				<AgentBar>
-					<Row flexFill>
-						{/* <Column>
-							<Avatar imgUrl={parseUrl(this.props.email.avatarUrl)} />
-						</Column> */}
-						<Column flexFill>
-							<Title>{this.props.name}</Title>
-							<Subtitle>Support hero</Subtitle>
-						</Column>
-						<Column flexFit>
-							{this.props.chatState === 'CHATTING' &&
-								<Row>
-									<IconButton onClick={ this.props.rateGood }>
-										<RateGoodIcon style={{
-											opacity: this.props.rate === 'good' ? '1' : '0.5'
-										}} />
-									</IconButton>
-									<IconButton onClick={ this.props.rateBad }>
-										<RateBadIcon style={{
-											opacity: this.props.rate === 'bad' ? '1' : '0.5'
-										}} />
-									</IconButton>
-								</Row>
-							}
-						</Column>
-					</Row>
-				</AgentBar>
-			)}
+		console.log(this.props.messages)
+		return (
 			<div
 				style={{
-					flexGrow: 1,
-					minHeight: 0,
+					display: 'flex',
+					flexDirection: 'column',
 					height: '100%',
 				}}
 			>
-				<MessageList active containScrollInSubtree>
-					{this.props.messages.map((message, index) => (
-								<Message
-									// avatarUrl={parseUrl(getAvatarForUser(message.authorId, users))}
-									date={message.parsedDate}
-									isOwn={message.userId === this.props.email}
-									key={message.id}
-								>
-									<Bubble isOwn={message.authorId === this.props.email}>
-										{message.title && <MessageTitle title={message.title} />}
-										{message.text && <MessageText>{message.text}</MessageText>}
-										{message.imageUrl && (
-											<MessageMedia>
-												<img src={message.imageUrl} />
-											</MessageMedia>
+				<TitleBar
+					rightIcons={[
+						<IconButton key="close" onClick={this.props.minimize}>
+							<CloseIcon />
+						</IconButton>,
+					]}
+					title="Welcome to LiveChat"
+				/>
+				{this.props.email && (
+					<AgentBar>
+						<Row flexFill>
+							{/* <Column>
+							<Avatar imgUrl={parseUrl(this.props.email.avatarUrl)} />
+						</Column> */}
+							<Column flexFill>
+								<Title>{this.props.name}</Title>
+								<Subtitle>Support hero</Subtitle>
+							</Column>
+							<Column flexFit>
+								{this.props.chatState === 'CHATTING' &&
+									<Row>
+										<IconButton onClick={this.props.rateGood}>
+											<RateGoodIcon style={{
+												opacity: this.props.rate === 'good' ? '1' : '0.5'
+											}} />
+										</IconButton>
+										<IconButton onClick={this.props.rateBad}>
+											<RateBadIcon style={{
+												opacity: this.props.rate === 'bad' ? '1' : '0.5'
+											}} />
+										</IconButton>
+									</Row>
+								}
+							</Column>
+						</Row>
+					</AgentBar>
+				)}
+				<div
+					style={{
+						flexGrow: 1,
+						minHeight: 0,
+						height: '100%',
+					}}
+				>
+					<MessageList active containScrollInSubtree>
+						{this.props.messages.map((message, index) => (
+							<Message
+								// avatarUrl={parseUrl(getAvatarForUser(message.authorId, users))}
+								date={message.parsedDate}
+								isOwn={message.userId === this.props.email}
+								key={message.id}
+							>
+								<Bubble isOwn={message.authorId === this.props.email}>
+									{message.title && <MessageTitle title={message.title} />}
+									{message.text && <MessageText>{message.text}</MessageText>}
+									{message.imageUrl && (
+										<MessageMedia>
+											<img src={message.imageUrl} />
+										</MessageMedia>
+									)}
+									{message.buttons &&
+										message.buttons.length !== 0 && (
+											<MessageButtons>
+												{message.buttons.map((button, buttonIndex) => (
+													<MessageButton
+														key={buttonIndex}
+														label={button.title}
+														onClick={() => {
+															this.props.sendMessage(button.postback)
+														}}
+													/>
+												))}
+											</MessageButtons>
 										)}
-										{message.buttons &&
-											message.buttons.length !== 0 && (
-												<MessageButtons>
-													{message.buttons.map((button, buttonIndex) => (
-														<MessageButton
-															key={buttonIndex}
-															label={button.title}
-															onClick={() => {
-																this.props.sendMessage(button.postback)
-															}}
-														/>
-													))}
-												</MessageButtons>
-										)}
-									</Bubble>
-								</Message>
-					))}
-				</MessageList>
+								</Bubble>
+							</Message>
+						))}
+					</MessageList>
+				</div>
+				<TextComposer onSend={this.props.sendMessage}>
+					<Row align="center">
+						<Fill>
+							<TextInput />
+						</Fill>
+						<Fit>
+							<SendButton />
+						</Fit>
+					</Row>
+				</TextComposer>
+				<div
+					style={{
+						textAlign: 'center',
+						fontSize: '.6em',
+						padding: '.4em',
+						background: '#fff',
+						color: '#888',
+					}}
+				>
+					{'Powered by LiveChat'}
+				</div>
 			</div>
-			<TextComposer onSend={this.props.onMessageSend}>
-				<Row align="center">
-					<Fill>
-						<TextInput />
-					</Fill>
-					<Fit>
-						<SendButton />
-					</Fit>
-				</Row>
-			</TextComposer>
-			<div
-				style={{
-					textAlign: 'center',
-					fontSize: '.6em',
-					padding: '.4em',
-					background: '#fff',
-					color: '#888',
-				}}
-			>
-				{'Powered by LiveChat'}
-			</div>
-		</div>
-	)
-}
+		)
+	}
 }
 
 
