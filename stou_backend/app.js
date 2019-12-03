6294b2f6470c9e28ebfe5cd917974847111766ef
Soumil Uppal <soumil.barca@gmail.com>
2019-12-03 18:20:30 -0500

    Minor fix

--- stou_backend/app.js
+++ stou_backend/app.js
@@ -485,7 +485,7 @@ app.use('/getreviewrating', function (req, res, next) {
                 tempOb = {
                   'customer': rows[i].FIRST_NAME + " " + rows[i].LAST_NAME,
                   'review': rows[i].REVIEW,
-                  'rating': rows[i].CUSTOMER_RATING
+                  'rating': rows[i].COOK_RATING
                 };
                 tempList.push(tempOb);
                 tempOb = {};
